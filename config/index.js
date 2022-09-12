c0fd64314c6dbcba774b8b3832eebffeee4c40aa
rungchan2 <leeh09077@gmail.com>
2022-09-13 00:08:49 +0900

    문제해결

--- config/index.js
+++ config/index.js
@@ -1,3 +1,7 @@
 export const DATABASE_ID = process.env.NOTION_DATABASE_ID
 export const TOKEN = process.env.NOTION_TOKEN
-export const DATABASE_ID_BOOKPOINT = process.env.NOTION_DATABASE_ID_BOOKPOINT
+export const DATABASE_ID_BOOKPOINT = "2e5e2e2406824675a438de144419fcb2"
+
+console.log(typeof TOKEN)
+console.log(typeof DATABASE_ID_BOOKPOINT)
+
